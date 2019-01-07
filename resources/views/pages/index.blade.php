f8e057576bb8c4a23e0143a9fefae9518edd87a8
jeroenbruijne <jeroentjezzz@hotmail.nl>
2019-01-07 01:45:32 +0100

    Laravel from scratch
    Gemaakt en gekeken tot 14:05.
    -Link: https://www.youtube.com/watch?v=bSG2YMqJJys
    -Mist nog npm & node.js voor automatische recompilling

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Dit is een testje man</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/hello', function () {
+//     return 'Hello nibba';
+// });
+
+// route::get('/users/{id}/{name}', function($id, $name){
+// 	return 'Hij heet '. $name . ' met ID ' . $id;
+// });
+
+route::get('/about', function(){
+	return view('pages.about');
+});
+
+
+Route::get('/',  'PagesController@index');
+Route::get('/about',  'PagesController@about');
+Route::get('/services',  'PagesController@services');
